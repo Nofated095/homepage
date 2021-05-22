38240d30441059b8b23e5d2ae3654b28b0994441
Restent Ou <53113178+Restent@users.noreply.github.com>
2021-05-22 19:13:48 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     // host: '0.0.0.0',  // 生成网页地址（本地调试使用）
     // port: '6666',  // 生成网页端口（本地调试使用）
     title: "Restent Blog",  // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
-    description: '只要有你们，就够了。',  // meta 中的描述文字，用于SEO
+    description: '谁都不一定平凡，而我停留在平凡。',  // meta 中的描述文字，用于SEO
     head: [
       ['link', { rel: 'icon', href: 'https://cdn.jsdelivr.net/gh/Restent/Restent@master/Favicon.ico' }],  //浏览器的标签栏的网页图标,基地址/docs/.vuepress/public
       ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }], //在移动端，搜索框在获得焦点时会放大
